371ddbfe9be6c93fca4f562c93424aab7fafdecd
GoodBoyCole <202495863+GoodBoyCole@users.noreply.github.com>
2025-02-05 16:16:54 +0000

    first

--- Dassl/dassl/modeling/backbone/models_vit.py
+++ Dassl/dassl/modeling/backbone/models_vit.py
@@ -9,4 +9,32 @@
 # DeiT: https://github.com/facebookresearch/deit
 # --------------------------------------------------------
 
-from functoo
+from functools import partial
+
+import torch
+import torch.nn as nn
+
+import timm.models.vision_transformer
+
+import torch.utils.model_zoo as model_zoo
+
+from .build import BACKBONE_REGISTRY
+from .backbone import Backbone
+
+model_url = "/home/coco/PycharmProjects/fedcoop/PromptFL-main/Dassl.pytorch/dassl/modeling/backbone/weight/mae_finetuned_vit_base.pth"
+
+class VisionTransformer(timm.models.vision_transformer.VisionTransformer):
+    """ Vision Transformer with support for global average pooling
+    """
+    def __init__(self, num_classes=100, global_pool=True, **kwargs):
+        super(VisionTransformer, self).__init__(**kwargs)
+        self.num_classes = num_classes
+        self.global_pool = global_pool
+        if self.global_pool:
+            norm_layer = kwargs['norm_layer']
+            embed_dim = kwargs['embed_dim']
+            self.fc_norm = norm_layer(embed_dim)
+
+            del self.norm  # remove the original norm
+        self._out_features = embed_dim
+        print("self._o
