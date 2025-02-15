62e6e790c6e3c23546a5c237c98a89b81bce8f04
GoodBoyCole <202495863+GoodBoyCole@users.noreply.github.com>
2025-02-15 20:41:29 +0000

    Remove white space

--- Dassl/dassl/modeling/ops/utils.py
+++ Dassl/dassl/modeling/ops/utils.py
@@ -3,4 +3,42 @@ import torch
 
 
 def sharpen_prob(p, temperature=2):
-    """Sharpening probability with a temper
+    """Sharpening probability with a temperature.
+
+    Args:
+        p (torch.Tensor): probability matrix (batch_size, n_classes)
+        temperature (float): temperature.
+    """
+    p = p.pow(temperature)
+    return p / p.sum(1, keepdim=True)
+
+
+def reverse_index(data, label):
+    """Reverse order."""
+    inv_idx = torch.arange(data.size(0) - 1, -1, -1).long()
+    return data[inv_idx], label[inv_idx]
+
+
+def shuffle_index(data, label):
+    """Shuffle order."""
+    rnd_idx = torch.randperm(data.shape[0])
+    return data[rnd_idx], label[rnd_idx]
+
+
+def create_onehot(label, num_classes):
+    """Create one-hot tensor.
+
+    We suggest using nn.functional.one_hot.
+
+    Args:
+        label (torch.Tensor): 1-D tensor.
+        num_classes (int): number of classes.
+    """
+    onehot = torch.zeros(label.shape[0], num_classes)
+    return onehot.scatter(1, label.unsqueeze(1).data.cpu(), 1)
+
+
+def sigmoid_rampup(current, rampup_length):
+    """Exponential rampup.
+
+    Arg
