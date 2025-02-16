c47fd077fd39ee547e4fbbb4cbd078c4cd310d52
GoodBoyCole <202495863+GoodBoyCole@users.noreply.github.com>
2025-02-16 09:36:07 +0000

    Fixed #21

--- Dassl/dassl/utils/logger.py
+++ Dassl/dassl/utils/logger.py
@@ -9,4 +9,42 @@ __all__ = ["Logger", "setup_logger"]
 
 
 class Logger:
-    """Write console output to exter
+    """Write console output to external text file.
+
+    Imported from `<https://github.com/Cysu/open-reid/blob/master/reid/utils/logging.py>`_
+
+    Args:
+        fpath (str): directory to save logging file.
+
+    Examples::
+       >>> import sys
+       >>> import os.path as osp
+       >>> save_dir = 'output/experiment-1'
+       >>> log_name = 'train.log'
+       >>> sys.stdout = Logger(osp.join(save_dir, log_name))
+    """
+
+    def __init__(self, fpath=None):
+        self.console = sys.stdout
+        self.file = None
+        if fpath is not None:
+            mkdir_if_missing(osp.dirname(fpath))
+            self.file = open(fpath, "w")
+
+    def __del__(self):
+        self.close()
+
+    def __enter__(self):
+        pass
+
+    def __exit__(self, *args):
+        self.close()
+
+    def write(self, msg):
+        self.console.write(msg)
+        if self.file is not None:
+            self.file.write(msg)
+
+    def flush(self):
+        self.console.flush()
+        if self.file is 
