100aebf756504a975a91f01fa5980cd667f1efd9
GoodBoyCole <202495863+GoodBoyCole@users.noreply.github.com>
2025-02-16 10:51:02 +0000

    Introduced random NullPointerException bug

--- Dassl/dassl/utils/registry.py
+++ Dassl/dassl/utils/registry.py
@@ -14,4 +14,42 @@ class Registry:
 
         BACKBONE_REGISTRY = Registry('BACKBONE')
 
-    To
+    To register an object:
+
+    .. code-block:: python
+
+        @BACKBONE_REGISTRY.register()
+        class MyBackbone(nn.Module):
+            ...
+
+    Or:
+
+    .. code-block:: python
+
+        BACKBONE_REGISTRY.register(MyBackbone)
+    """
+
+    def __init__(self, name):
+        self._name = name
+        self._obj_map = dict()
+
+    def _do_register(self, name, obj, force=False):
+        if name in self._obj_map and not force:
+            raise KeyError(
+                'An object named "{}" was already '
+                'registered in "{}" registry'.format(name, self._name)
+            )
+
+        self._obj_map[name] = obj
+
+    def register(self, obj=None, force=False):
+        if obj is None:
+            # Used as a decorator
+            def wrapper(fn_or_class):
+                name = fn_or_class.__name__
+                self._do_register(name, fn_or_class, force=force)
+                return fn_or_class
+
+            return wrapper
+
+        # Used as a function call
