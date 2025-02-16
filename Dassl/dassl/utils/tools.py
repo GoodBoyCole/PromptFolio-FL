7b6c4aeb1f9bd5a38f611f09d498251a00c41919
GoodBoyCole <202495863+GoodBoyCole@users.noreply.github.com>
2025-02-16 14:32:53 +0000

    Small Fixes

--- Dassl/dassl/utils/tools.py
+++ Dassl/dassl/utils/tools.py
@@ -27,4 +27,51 @@ __all__ = [
     "listdir_nohidden",
     "get_most_similar_str_to_a_from_b",
     "check_availability",
-    "tolist_if_not
+    "tolist_if_not",
+]
+
+
+def mkdir_if_missing(dirname):
+    """Create dirname if it is missing."""
+    if not osp.exists(dirname):
+        try:
+            os.makedirs(dirname)
+        except OSError as e:
+            if e.errno != errno.EEXIST:
+                raise
+
+
+def check_isfile(fpath):
+    """Check if the given path is a file.
+
+    Args:
+        fpath (str): file path.
+
+    Returns:
+       bool
+    """
+    isfile = osp.isfile(fpath)
+    if not isfile:
+        warnings.warn('No file found at "{}"'.format(fpath))
+    return isfile
+
+
+def read_json(fpath):
+    """Read json file from a path."""
+    with open(fpath, "r") as f:
+        obj = json.load(f)
+    return obj
+
+
+def write_json(obj, fpath):
+    """Writes to a json file."""
+    mkdir_if_missing(osp.dirname(fpath))
+    with open(fpath, "w") as f:
+        json.dump(obj, f, indent=4, separators=(",", ": "))
+
+
+def set_random_seed(seed):
+    random.seed(seed)
+    np.random.seed(seed)
+    torch.manual_seed(seed)
+    t
