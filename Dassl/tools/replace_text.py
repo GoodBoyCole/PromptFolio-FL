19e10f671e4275ae74d89024d095472e9926c8e5
GoodBoyCole <202495863+GoodBoyCole@users.noreply.github.com>
2025-02-18 21:43:58 +0000

    adding license headers

--- Dassl/tools/replace_text.py
+++ Dassl/tools/replace_text.py
@@ -16,4 +16,30 @@ def is_python_file(filename):
 
 def update_file(filename, text_to_search, replacement_text):
     print("Processing {}".format(filename))
-    with fileinput.FileInput(filename, inplace=True, backup="") as
+    with fileinput.FileInput(filename, inplace=True, backup="") as file:
+        for line in file:
+            print(line.replace(text_to_search, replacement_text), end="")
+
+
+def recursive_update(directory, text_to_search, replacement_text):
+    filenames = glob.glob(osp.join(directory, "*"))
+
+    for filename in filenames:
+        if osp.isfile(filename):
+            if not is_python_file(filename):
+                continue
+            update_file(filename, text_to_search, replacement_text)
+        elif osp.isdir(filename):
+            recursive_update(filename, text_to_search, replacement_text)
+        else:
+            raise NotImplementedError
+
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "file_or_dir", type=str, help="path to file or directory"
+    )
+    parser.add_argument("text_to_search", type=str, help="name to be replaced")
+    parser.add_argument("replacement_text", type=str, help="new name")
+    pa
