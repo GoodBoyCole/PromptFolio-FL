f3ba3e802f0b4cc1be005152de6c4261212cd932
GoodBoyCole <202495863+GoodBoyCole@users.noreply.github.com>
2025-03-04 17:33:16 +0000

    CircleCI config

--- clip/model.py
+++ clip/model.py
@@ -73,4 +73,27 @@ class AttentionPool2d(nn.Module):
         x, _ = F.multi_head_attention_forward(
             query=x, key=x, value=x,
             embed_dim_to_check=x.shape[-1],
- 
+            num_heads=self.num_heads,
+            q_proj_weight=self.q_proj.weight,
+            k_proj_weight=self.k_proj.weight,
+            v_proj_weight=self.v_proj.weight,
+            in_proj_weight=None,
+            in_proj_bias=torch.cat([self.q_proj.bias, self.k_proj.bias, self.v_proj.bias]),
+            bias_k=None,
+            bias_v=None,
+            add_zero_attn=False,
+            dropout_p=0,
+            out_proj_weight=self.c_proj.weight,
+            out_proj_bias=self.c_proj.bias,
+            use_separate_proj_weight=True,
+            training=self.training,
+            need_weights=False
+        )
+
+        return x
+
+# class AttentionPool2d(nn.Module):
+#     def __init__(self, spacial_dim: int, embed_dim: int, num_heads: int, output_dim: int = None):
+#         super().__init__()
+#         self.positional_embedding = nn.Parameter(torch.randn(spacial_dim ** 2 + 1, embed_dim) / embed_dim ** 0.5)
+#         self.k
