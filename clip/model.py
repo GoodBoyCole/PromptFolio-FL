52f89d07a5cc0b95ebee7ca59cb3a496d4d66345
GoodBoyCole <202495863+GoodBoyCole@users.noreply.github.com>
2025-03-12 10:13:00 +0000

    bc

--- clip/model.py
+++ clip/model.py
@@ -325,4 +325,27 @@ class VisionTransformer(nn.Module):
 
         self.transformer = Transformer(width, layers, heads, design_details=design_details)
 
-        self
+        self.ln_post = LayerNorm(width)
+        self.proj = nn.Parameter(scale * torch.randn(width, output_dim))
+
+    def forward(self, x: torch.Tensor):
+        x = self.conv1(x)  # shape = [*, width, grid, grid]
+        x = x.reshape(x.shape[0], x.shape[1], -1)  # shape = [*, width, grid ** 2]
+        x = x.permute(0, 2, 1)  # shape = [*, grid ** 2, width]
+        x = torch.cat([self.class_embedding.to(x.dtype) + torch.zeros(x.shape[0], 1, x.shape[-1], dtype=x.dtype, device=x.device), x], dim=1)  # shape = [*, grid ** 2 + 1, width]
+        x = x + self.positional_embedding.to(x.dtype)
+        x = self.ln_pre(x)
+
+        x = x.permute(1, 0, 2)  # NLD -> LND
+        x = self.transformer(x)
+        x = x.permute(1, 0, 2)  # LND -> NLD
+
+        x = self.ln_post(x[:, 0, :])
+
+        if self.proj is not None:
+            x = x @ self.proj
+
+        return x
+
+class TPGVisionTransformer(VisionTransformer):
+    def __init__(self, input_resolution: int, patch_size: int, width: int, layers: int, heads: 
