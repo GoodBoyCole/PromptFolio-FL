efd79ecd35bd111e47a0568c3fc8f2242a7d9354
GoodBoyCole <202495863+GoodBoyCole@users.noreply.github.com>
2025-05-01 08:41:52 +0000

    delete useless file

--- datasets/caltech101.py
+++ datasets/caltech101.py
@@ -2,4 +2,32 @@ import os
 import pickle
 
 # from Dassl.dassl.data.datasets import DATASET_REGISTRY, Datum, DatasetBase
-from Dassl.dass
+from Dassl.dassl.data.datasets.base_dataset import DatasetBase
+from Dassl.dassl.utils import mkdir_if_missing
+
+from .oxford_pets import OxfordPets
+from .dtd import DescribableTextures as DTD
+
+IGNORED = ["BACKGROUND_Google", "Faces_easy"]
+NEW_CNAMES = {
+    "airplanes": "airplane",
+    "Faces": "face",
+    "Leopards": "leopard",
+    "Motorbikes": "motorbike",
+}
+
+
+# @DATASET_REGISTRY.register()
+class Caltech101(DatasetBase):
+    dataset_dir = "caltech-101"
+
+    def __init__(self, cfg):
+        root = os.path.abspath(os.path.expanduser(cfg.DATASET.ROOT))
+        self.dataset_dir = os.path.join(root, self.dataset_dir)
+        self.image_dir = os.path.join(self.dataset_dir, "101_ObjectCategories")
+        self.split_path = os.path.join(self.dataset_dir, "split_zhou_Caltech101.json")
+        self.split_fewshot_dir = os.path.join(self.dataset_dir, "split_fewshot_fed")
+        self.baseline_dir = os.path.join(self.dataset_dir, "baseline")
+        mkdir_if_missing(self.split_fewshot_dir)
+
+    
