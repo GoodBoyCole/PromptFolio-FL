f71a7f4957d06094108689f261e8475a56211671
GoodBoyCole <202495863+GoodBoyCole@users.noreply.github.com>
2025-05-01 15:02:33 +0000

    v1.1.4

--- datasets/cifar100.py
+++ datasets/cifar100.py
@@ -1,3 +1,23 @@
 import os
 
 # from Dassl.dassl.data.datasets import DATASET_REGISTRY, Datum, DatasetBase
+# from Dassl.dassl.data.datasets import DatasetBase
+from datasplit import partition_data
+
+# @DATASET_REGISTRY.register()
+class Cifar100():
+    dataset_dir = "cifar-100"
+    def __init__(self, cfg):
+        root = os.path.abspath(os.path.expanduser(cfg.DATASET.ROOT))
+        self.dataset_dir = os.path.join(root, self.dataset_dir)
+        self.num_classes = 100
+
+        federated_train_x = [[] for i in range(cfg.DATASET.USERS)]
+        federated_test_x = [[] for i in range(cfg.DATASET.USERS)]
+
+        data_train, data_test, lab2cname, classnames, net_dataidx_map_train, net_dataidx_map_test, traindata_cls_counts, testdata_cls_counts = partition_data(
+            'cifar100', self.dataset_dir, cfg.DATASET.PARTITION, cfg.DATASET.USERS, beta=cfg.DATASET.BETA, logdir="./logs/")
+        for net_id in range(cfg.DATASET.USERS):
+            dataidxs_train = net_dataidx_map_train[net_id]
+            dataidxs_test = net_dataidx_map_test[net_id]
+     
