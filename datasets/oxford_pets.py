83280de13cf3ca096f7ae88d2b0e75869b055d26
GoodBoyCole <202495863+GoodBoyCole@users.noreply.github.com>
2025-05-04 10:47:04 +0000

    Updated composer lock

--- datasets/oxford_pets.py
+++ datasets/oxford_pets.py
@@ -11,3 +11,22 @@ from Dassl.dassl.utils import read_json, write_json, mkdir_if_missing
 
 # @DATASET_REGISTRY.register()
 class OxfordPets(DatasetBase):
+
+    dataset_dir = "oxford_pets"
+
+    def __init__(self, cfg):
+        root = os.path.abspath(os.path.expanduser(cfg.DATASET.ROOT))
+        self.dataset_dir = os.path.join(root, self.dataset_dir)
+        self.image_dir = os.path.join(self.dataset_dir, "images")
+        self.anno_dir = os.path.join(self.dataset_dir, "annotations")
+        self.split_path = os.path.join(self.dataset_dir, "split_zhou_OxfordPets.json")
+        self.split_fewshot_dir = os.path.join(self.dataset_dir, "split_fewshot_fed")
+        self.baseline_dir = os.path.join(self.dataset_dir, "baseline")
+        mkdir_if_missing(self.split_fewshot_dir)
+
+        if os.path.exists(self.split_path):
+            total_train, val, test = self.read_split(self.split_path, self.image_dir)
+        else:
+            trainval = self.read_data(split_file="trainval.txt")
+            test = self.read_data(split_file="test.txt")
+            total_train, val = self.split_tr
