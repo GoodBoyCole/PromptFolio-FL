1180c4583b6152b6c93a39235b5b74dcd0d30c02
GoodBoyCole <202495863+GoodBoyCole@users.noreply.github.com>
2025-05-27 20:36:48 +0000

    \t* gcc.pot: Regenerate.

--- trainers/FedTPG.py
+++ trainers/FedTPG.py
@@ -9,4 +9,44 @@ from torch.cuda.amp import GradScaler, autocast
 from Dassl.dassl.engine.trainer import TrainerX
 from Dassl.dassl.metrics import compute_accuracy
 from Dassl.dassl.utils import load_pretrained_weights, load_checkpoint
-from Dassl.dassl.opt
+from Dassl.dassl.optim import build_optimizer, build_lr_scheduler
+
+from clip import clip
+from clip.simple_tokenizer import SimpleTokenizer as _Tokenizer
+
+_tokenizer = _Tokenizer()
+
+
+
+
+def load_clip_to_cpu(cfg):
+    backbone_name = cfg.MODEL.BACKBONE.NAME
+    url = clip._MODELS[backbone_name]
+    model_path = clip._download(url)
+
+    try:
+        model = torch.jit.load(model_path, map_location="cpu").eval()
+        state_dict = None
+    except RuntimeError:
+        state_dict = torch.load(model_path, map_location="cpu")
+
+    design_details = {
+        "trainer": 'FedTPG',
+        "vision_depth": 0,
+        "language_depth": 0,
+        "vision_ctx": 0,
+        "language_ctx": 0
+    }
+
+    model = clip.build_model(state_dict or model.state_dict(), design_details)
+    return model
+
+
+def exists(val):
+    return val is not None
+
+
+class PreNorm(nn.Module):
+    def __init__(self, dim, fn, context_dim=None):
+        super().__init__()
+        self.fn = fn
