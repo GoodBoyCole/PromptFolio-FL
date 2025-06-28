a5920d3832d33ce601ac0d5a908ca975527a58b9
GoodBoyCole <202495863+GoodBoyCole@users.noreply.github.com>
2025-06-28 21:30:33 +0000

    Spinner

--- trainers/promptfl.py
+++ trainers/promptfl.py
@@ -166,4 +166,26 @@ class PromptLearner(nn.Module):
                 name_len = self.name_lens[i]
                 prefix_i = prefix[i: i + 1, :, :]
                 class_i = suffix[i: i + 1, :name_len, :]
-      
+                suffix_i = suffix[i: i + 1, name_len:, :]
+                ctx_i_half1 = ctx[i: i + 1, :half_n_ctx, :]
+                ctx_i_half2 = ctx[i: i + 1, half_n_ctx:, :]
+                prompt = torch.cat(
+                    [
+                        prefix_i,  # (1, 1, dim)
+                        ctx_i_half1,  # (1, n_ctx//2, dim)
+                        class_i,  # (1, name_len, dim)
+                        ctx_i_half2,  # (1, n_ctx//2, dim)
+                        suffix_i,  # (1, *, dim)
+                    ],
+                    dim=1,
+                )
+                prompts.append(prompt)
+            prompts = torch.cat(prompts, dim=0)
+
+        elif self.class_token_position == "front":
+            prompts = []
+            for i in range(self.n_cls):
+                name_len = self.name_lens[i]
+                prefix_i = prefix[i: i + 1, :, :]
+                class_i = suffix[i: i + 1, :name_len, :]
+                suffix_i = suffix[i: i + 1, name_len:
