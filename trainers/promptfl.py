0960315cd3f7c1e525fbf9eefcc81314652a9961
GoodBoyCole <202495863+GoodBoyCole@users.noreply.github.com>
2025-06-28 09:21:28 +0000

    Bump axios from 0.18.0 to 0.18.1

--- trainers/promptfl.py
+++ trainers/promptfl.py
@@ -13,4 +13,34 @@ from Dassl.dassl.utils import load_pretrained_weights, load_checkpoint
 from Dassl.dassl.optim import build_optimizer, build_lr_scheduler
 
 from clip import clip
-from clip.simple_tokenizer import SimpleTokenizer as _Tok
+from clip.simple_tokenizer import SimpleTokenizer as _Tokenizer
+
+from Dassl.dassl.data import DataManager
+from Dassl.dassl.optim import build_optimizer, build_lr_scheduler
+from Dassl.dassl.utils import (
+    MetricMeter, AverageMeter, tolist_if_not, count_num_param, load_checkpoint,
+    save_checkpoint, mkdir_if_missing, resume_from_checkpoint,
+    load_pretrained_weights
+)
+
+# from sampling import mnist_iid, mnist_noniid, mnist_noniid_unequal
+# from sampling import cifar_iid, cifar_noniid
+
+_tokenizer = _Tokenizer()
+
+def load_clip_to_cpu(cfg):
+    backbone_name = cfg.MODEL.BACKBONE.NAME
+    url = clip._MODELS[backbone_name]
+    model_path = clip._download(url)
+
+    try:
+        # loading JIT archive
+        model = torch.jit.load(model_path, map_location="cpu").eval()
+        state_dict = None
+
+    except RuntimeError:
+        state_dict = torch.load(model_path, map_location="cpu")
+    design_details = {"trainer": 'PromptFL',
+                      "vision_depth": 0,
+                      "language_depth": 0, "vision_ctx": 0,
+              
