76ee197e012c1a65fc365d2b3bc6c7a5cd91cb60
GoodBoyCole <202495863+GoodBoyCole@users.noreply.github.com>
2025-06-28 10:08:51 +0000

    update godep

--- trainers/promptfl.py
+++ trainers/promptfl.py
@@ -43,4 +43,29 @@ def load_clip_to_cpu(cfg):
     design_details = {"trainer": 'PromptFL',
                       "vision_depth": 0,
                       "language_depth": 0, "vision_ctx": 0,
-              
+                      "language_ctx": 0}
+
+    model = clip.build_model(state_dict or model.state_dict(), design_details)
+
+    return model
+
+
+class TextEncoder(nn.Module):
+    def __init__(self, clip_model):
+        super().__init__()
+        self.transformer = clip_model.transformer
+        self.positional_embedding = clip_model.positional_embedding
+        self.ln_final = clip_model.ln_final
+        self.text_projection = clip_model.text_projection
+        self.dtype = clip_model.dtype
+
+    def forward(self, prompts, tokenized_prompts):
+        x = prompts + self.positional_embedding.type(self.dtype)
+        x = x.permute(1, 0, 2)  # NLD -> LND
+        x = self.transformer(x)
+        x = x.permute(1, 0, 2)  # LND -> NLD
+        x = self.ln_final(x).type(self.dtype)
+
+        # x.shape = [batch_size, n_ctx, transformer.width]
+        # take features from the eot embedding (eot_token is the highest number in each sequence)
+        x = x[torch.arange(x.shape[0]), 
