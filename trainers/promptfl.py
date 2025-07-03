a4f6991783349afe14279de02c457ffb585e459e
GoodBoyCole <202495863+GoodBoyCole@users.noreply.github.com>
2025-07-03 12:38:55 +0000

    Initialize repo

--- trainers/promptfl.py
+++ trainers/promptfl.py
@@ -215,4 +215,33 @@ class CustomCLIP(nn.Module):
         self.tokenized_prompts = self.prompt_learner.tokenized_prompts
         self.image_encoder = clip_model.visual
         self.text_encoder = TextEncoder(clip_model)
-        self.logit
+        self.logit_scale = clip_model.logit_scale
+        self.dtype = clip_model.dtype
+
+    def forward(self, image):
+        image_features = self.image_encoder(image.type(self.dtype))
+
+        prompts = self.prompt_learner()
+        tokenized_prompts = self.tokenized_prompts
+        text_features = self.text_encoder(prompts, tokenized_prompts)
+
+        image_features = image_features / image_features.norm(dim=-1, keepdim=True)
+        text_features = text_features / text_features.norm(dim=-1, keepdim=True)
+
+        logit_scale = self.logit_scale.exp()
+        logits = logit_scale * image_features @ text_features.t()
+
+        return logits
+
+
+# @TRAINER_REGISTRY.register("PromptFL")
+class PromptFL(TrainerX):
+
+    def check_cfg(self, cfg):
+        assert cfg.TRAINER.PROMPTFL.PREC in ["fp16", "fp32", "amp"]
+
+    def build_model(self):
+        cfg = self.cfg
+        classnames = self.dm.dataset.classnames
+
+        print(f"Loading CLIP (backbone: {cfg.MODEL.BA
