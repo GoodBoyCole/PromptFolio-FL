cd5035ebf2f4c664de4e40dd3b3b7f8351718383
GoodBoyCole <202495863+GoodBoyCole@users.noreply.github.com>
2025-07-08 13:28:49 +0000

    adjusting

--- trainers/promptflFedAMP.py
+++ trainers/promptflFedAMP.py
@@ -27,4 +27,34 @@ class PromptFLFedAMP(PromptFL):
             self.optim.zero_grad()
             self.scaler.scale(loss).backward()
             self.scaler.step(self.optim)
-            s
+            self.scaler.update()
+        else:
+            output = self.model(image)
+            loss = F.cross_entropy(output, label)
+            if self.u is not None:
+                lam = self.cfg.TRAINER.PROMPTFLFEDAMP.LAMBDA
+                loss += lam * torch.norm(self.model.state_dict()["prompt_learner.ctx"] - self.u, p=2)
+            self.model_backward_and_update(loss)
+
+        loss_summary = {
+            "loss": loss.item(),
+            "acc": compute_accuracy(output, label)[0].item(),
+        }
+
+        if (self.batch_idx + 1) == self.num_batches:
+            self.update_lr()
+
+        return loss_summary
+
+    def fed_upload_model(self, idx):
+        super().fed_upload_model(idx)
+        self.local_info[idx].pop("u", None)
+
+    def fed_aggregate_model(self, idx_users):
+        global_list = []
+        sigma = self.cfg.TRAINER.PROMPTFLFEDAMP.SIGMA
+        for idx in idx_users:
+            global_list.append(self.local_info[idx]["prompt_learner.ctx"])
+
+        def attention_func(dist):
+            return 1 
